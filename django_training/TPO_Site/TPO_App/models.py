1f9f91a9d7cf2e4ad5627a3f3255c2dfb4a8c76a
NehaMondal <nehamondal360@gmail.com>
2021-06-04 19:09:04 +0530

    models added

--- django_training/TPO_Site/TPO_App/models.py
+++ django_training/TPO_Site/TPO_App/models.py
@@ -22,7 +22,7 @@ class UserInfoModel(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     roll_no = models.CharField(max_length=10,unique=True)
     passing_year = models.PositiveIntegerField(null=False)
-    branch= models.CharField(max_length=10,choices=BRANCH, default='CS', blank = True)
+    branch= models.CharField(max_length=10,choices=BRANCH, default='CS', blank = False)
     course = models.CharField(max_length=5,choices=COURSE, default='BTECH')
     college = models.CharField(max_length=100, blank=False)
     dob = models.DateField()
@@ -41,3 +41,56 @@ class UserInfoModel(models.Model):
     def __str__(self):
         return self.user.username
 
+class UserMarksModel(models.Model):
+    user = models.ForeignKey(UserInfoModel, on_delete=models.CASCADE, related_name="marks")
+    tenth_obt_marks = models.PositiveSmallIntegerField()
+    tenth_total_marks = models.PositiveSmallIntegerField()
+    twelve_obt_marks = models.PositiveSmallIntegerField()
+    twelve_total_marks = models.PositiveSmallIntegerField()
+    jee_rank = models.PositiveSmallIntegerField(null=True)
+    first_sem_marks = models.PositiveSmallIntegerField(null=True)
+    second_sem_marks = models.PositiveSmallIntegerField(null=True)
+    third_sem_marks = models.PositiveSmallIntegerField(null=True)
+    fourth_sem_marks = models.PositiveSmallIntegerField(null=True)
+    fifth_sem_marks = models.PositiveSmallIntegerField(null=True)
+    sixth_sem_marks = models.PositiveSmallIntegerField(null=True)
+    seventh_sem_marks = models.PositiveSmallIntegerField(null=True)
+    aggregates = models.PositiveSmallIntegerField(null=True)
+
+    def __str__(self):
+        return self.aggregates
+
+
+class TrainingInfoModel(models.Model):
+    Mode = (
+        ('Offline', 'Offline'),
+        ('Online', 'Online')
+    )
+    user = models.ForeignKey(UserInfoModel, on_delete=models.CASCADE, related_name="training_detail")
+    technology= models.CharField(max_length=80)
+    project = models.CharField(max_length=50)
+    training_mode= models.CharField(max_length=7,choices=Mode, default="Offline", blank=False)
+    institute_name = models.CharField(max_length=40)
+    institute_address = models.CharField(max_length=100)
+    institute_number = models.PositiveSmallIntegerField(null=True, blank=True)
+    training_duration = models.PositiveSmallIntegerField(null = False)
+
+    def __str__(self):
+        return self.technology
+
+
+
+class DocumentsModel(models.Model):
+    user = models.ForeignKey(UserInfoModel, on_delete=models.CASCADE, related_name="document_proof")
+    tenth_dmc = models.FileField(upload_to="documents/tenth_dmc")
+    twelvth_dmc = models.FileField(upload_to="documents/twelvth_dmc")
+    first_sem_dmc = models.FileField(upload_to="documents/first_sem")
+    second_sem_dmc = models.FileField(upload_to="documents/second_sem")
+    third_sem_dmc = models.FileField(upload_to="documents/third_sem")
+    fourth_sem_dmc = models.FileField(upload_to="documents/fourth_sem")
+    fifth_sem_dmc = models.FileField(upload_to="documents/fifth_sem")
+    sixth_sem_dmc = models.FileField(upload_to="documents/sixth_sem")
+    seventh_sem = models.FileField(upload_to="documents/seventh_sem")
+
+    def __str__(self):
+        return self.user.username
--- django_training/TPO_Site/TPO_Site/settings.py
+++ django_training/TPO_Site/TPO_Site/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
+MEDIA_DIR = os.path.join(BASE_DIR,'media')
 
 
 # Quick-start development settings - unsuitable for production
@@ -121,4 +122,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
-LOGOUT_REDIRECT_URL='logout'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = MEDIA_DIR
+LOGIN_URL = '/login/'
+LOGOUT_REDIRECT_URL= '/logout/'
